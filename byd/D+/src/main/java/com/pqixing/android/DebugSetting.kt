52b421a25559fa652668ab0322519c407547a073
pengqixing <pqixing@163.com>
2023-02-16 20:09:43 +0800

    增加直接访问系统的api

--- byd/D+/src/main/java/com/pqixing/android/DebugSetting.kt
+++ byd/D+/src/main/java/com/pqixing/android/DebugSetting.kt
@@ -1,6 +1,8 @@
 package com.pqixing.android
 
 import android.app.Activity
+import android.app.AlertDialog
+import android.hardware.bydauto.setting.BYDAutoSettingDevice
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -20,11 +22,32 @@ class DebugSetting : DSetting("调试开关") {
         view.findViewById<View>(R.id.tv_radar).setOnClickListener {
             result.text = BYDAutoInstrumentUtils.getAllRadarDistance().joinToString()
         }
+        view.findViewById<View>(R.id.tv_setting).setOnClickListener {
+            onSettingClick(activity, result)
+        }
         view.findViewById<CheckBox>(R.id.ib_air_control).setOnCheckedChangeListener { _, check ->
             result.text = BYDAutoInstrumentUtils.setWirelessCharging(if (check) 1 else 0).toString()
         }
 
         return view
+    }
+
+    private fun onSettingClick(ac: Activity, result: TextView) {
+        val settins = arrayOf("后视镜状态", "空调", "后备箱")
+        AlertDialog.Builder(ac).setTitle("设置项目").setSingleChoiceItems(settins, 0) { d, i ->
+            d.dismiss()
+
+            kotlin.runCatching {
+                val instance = BYDAutoSettingDevice.getInstance(ac) ?: return@setSingleChoiceItems
+                val r = when (i) {
+                    0 -> "autoExternalRearMirrorFollowUpSwitch=${instance.autoExternalRearMirrorFollowUpSwitch} ; rearMirrorFlip=${instance.rearMirrorFlip} ; flipAngle=${instance.leftViewMirrorFlipAngle} ,${instance.rightViewMirrorFlipAngle} ; rearViewMirrorAutoFoldMode=${instance.rearViewMirrorAutoFoldMode} ; rearViewMirrorFlip=${instance.rearViewMirrorFlip} ;"
+                    1 -> "acAutoWindLevel=${instance.acAutoWindLevel} ; acAutoAir =${instance.acAutoAir}"
+                    2 -> "backDoorOpenedHeight=${instance.backDoorOpenedHeight} ; backDoorElectricMode=${instance.backDoorElectricMode} ; backDoorElectricModeOnlineState=${instance.backDoorElectricModeOnlineState}"
+                    else -> ""
+                }.toString()
+                result.text = "${settins.getOrNull(i)} : $r"
+            }
+        }.show()
     }
 
 }
---
+++ byd/compile/src/main/java/android/hardware/bydauto/setting/BYDAutoSettingDevice.java
@@ -0,0 +1,1070 @@
+package android.hardware.bydauto.setting;
+
+import android.content.Context;
+import android.hardware.bydauto.AbsBYDAutoDevice;
+
+/* loaded from: classes.dex */
+public final class BYDAutoSettingDevice extends AbsBYDAutoDevice {
+
+    public static synchronized BYDAutoSettingDevice getInstance(Context context) {
+        return null;
+    }
+
+    public BYDAutoSettingDevice(Context context) {
+        super(context);
+    }
+
+    public int factoryResetAll(int i) {
+        return 0;
+    }
+
+
+    public int getACAutoAir() {
+        return 0;
+    }
+
+
+    public int getACAutoWindLevel() {
+        return 0;
+    }
+
+
+    public int getACBTWind() {
+        return 0;
+    }
+
+
+    public int getACPauseCycle() {
+        return 0;
+    }
+
+
+    public int getACTunnelCycle() {
+        return 0;
+    }
+
+
+    public int getAutoExternalRearMirrorFollowUpSwitch() {
+        return 0;
+    }
+
+
+    public int getAutoLock() {
+        return 0;
+    }
+
+
+    public int getAutoLockSwitch() {
+        return 0;
+    }
+
+
+    public int getAutoLockTime() {
+        return 0;
+    }
+
+
+    public int getAutoRainWiperState() {
+        return 0;
+    }
+
+
+    public int getBTCallReductionWindMenuState() {
+        return 0;
+    }
+
+
+    public byte[] getBTMacAddr() {
+        return new byte[0];
+    }
+
+
+    public int getBackDoorElectricMode() {
+        return 0;
+    }
+
+
+    public int getBackDoorElectricModeOnlineState() {
+        return 0;
+    }
+
+
+    public int getBackDoorOpenedHeight() {
+        return 0;
+    }
+
+
+    public int getBackHomeLightDelay() {
+        return 0;
+    }
+
+
+    public int getBackHomeLightDelayValue() {
+        return 0;
+    }
+
+
+    public int getBackRowControl() {
+        return 0;
+    }
+
+
+    public int getBasePadAutoRise() {
+        return 0;
+    }
+
+
+    public int getBasePadAutoRiseState() {
+        return 0;
+    }
+
+
+    public int getCarRecorderRecording() {
+        return 0;
+    }
+
+
+    public int getChargingPort() {
+        return 0;
+    }
+
+
+    public int getCourtesyLampTime() {
+        return 0;
+    }
+
+
+    public int getDoorLock() {
+        return 0;
+    }
+
+
+    public int getDriveConfig() {
+        return 0;
+    }
+
+
+    public int getDriveDoorCloseInd() {
+        return 0;
+    }
+
+
+    public int getDriverSeatAutoReturn() {
+        return 0;
+    }
+
+
+    public int getDriverSeatBack() {
+        return 0;
+    }
+
+
+    public int getDrivingRecorder() {
+        return 0;
+    }
+
+
+    public int getDrivingRecorderSwitchState() {
+        return 0;
+    }
+
+
+    public int getDualTempAreaAcOnlineState() {
+        return 0;
+    }
+
+
+    public int getElecHandbrakeState() {
+        return 0;
+    }
+
+
+    public int getEnergyFeedback() {
+        return 0;
+    }
+
+
+    public int getEngineOilExitUpdateState() {
+        return 0;
+    }
+
+
+    public int getEngineOilLevel() {
+        return 0;
+    }
+
+
+    public int getEngineOilUpdateSignal() {
+        return 0;
+    }
+
+
+    public int getExternalRearMirrorAutoFoldSetting() {
+        return 0;
+    }
+
+
+    public int[] getFeatureList() {
+        return new int[0];
+    }
+
+
+    public int getHomeLightTime() {
+        return 0;
+    }
+
+
+    public int getIALArea() {
+        return 0;
+    }
+
+
+    public int getIALBrightness() {
+        return 0;
+    }
+
+
+    public int getIALColor() {
+        return 0;
+    }
+
+
+    public int getIKEYBTLowPowerMode() {
+        return 0;
+    }
+
+
+    public int getILDuration() {
+        return 0;
+    }
+
+
+    public int getINSTheme() {
+        return 0;
+    }
+
+
+    public int getInsBacklightCtl() {
+        return 0;
+    }
+
+
+    public int getInsideLightDoorState() {
+        return 0;
+    }
+
+
+    public int getInsideRearMirrorScreenSwitchState() {
+        return 0;
+    }
+
+
+    public int getIntelligentVoiceIND() {
+        return 0;
+    }
+
+
+    public int getKeyPowerLowInd() {
+        return 0;
+    }
+
+
+    public int getLPSwitchDownwindow() {
+        return 0;
+    }
+
+
+    public int getLPSwitchUpwindow() {
+        return 0;
+    }
+
+
+    public int getLanguage() {
+        return 0;
+    }
+
+
+    public int getLeaveCarPInd() {
+        return 0;
+    }
+
+
+    public int getLeaveHomeLightTime() {
+        return 0;
+    }
+
+
+    public int getLeftHeadlampLevel() {
+        return 0;
+    }
+
+
+    public int getLeftHomeLightDelay() {
+        return 0;
+    }
+
+
+    public int getLeftHomeLightDelayValue() {
+        return 0;
+    }
+
+
+    public int getLeftViewMirrorFlipAngle() {
+        return 0;
+    }
+
+
+    public int getLockCarRiseWindow() {
+        return 0;
+    }
+
+
+    public int getLockOff() {
+        return 0;
+    }
+
+
+    public int getLockUpwindow() {
+        return 0;
+    }
+
+
+    public int getLongPressUnlockWindow() {
+        return 0;
+    }
+
+
+    public int getLowBatteryInd() {
+        return 0;
+    }
+
+
+    public int getLowOilInd() {
+        return 0;
+    }
+
+
+    public int getMaintainRemindState() {
+        return 0;
+    }
+
+
+    public int getMicroSwitchLockWindowState() {
+        return 0;
+    }
+
+
+    public int getMicroSwitchUnlockWindowState() {
+        return 0;
+    }
+
+
+    public int getMissKeyInd() {
+        return 0;
+    }
+
+
+    public int getMultimediaMotorTemperatureState() {
+        return 0;
+    }
+
+
+    public int getNightDRLightOnInd() {
+        return 0;
+    }
+
+
+    public int getNightDriveLightOnReminder() {
+        return 0;
+    }
+
+
+    public int getOverspeedLock() {
+        return 0;
+    }
+
+
+    public int getOverspeedLockingState() {
+        return 0;
+    }
+
+
+    public int getPM25Power() {
+        return 0;
+    }
+
+
+    public int getPM25Switch() {
+        return 0;
+    }
+
+
+    public int getPM25SwitchCheck() {
+        return 0;
+    }
+
+
+    public int getPM25TimeCheck() {
+        return 0;
+    }
+
+
+    public int getPanoramaSunRoofOnlineState() {
+        return 0;
+    }
+
+
+    public int getParkBrakeInd() {
+        return 0;
+    }
+
+
+    public int getRearAcOnlineState() {
+        return 0;
+    }
+
+
+    public int getRearMirrorFlip() {
+        return 0;
+    }
+
+
+    public int getRearViewMirrorAutoFoldMode() {
+        return 0;
+    }
+
+
+    public int getRearViewMirrorAutoFoldModeOnlineState() {
+        return 0;
+    }
+
+
+    public int getRearViewMirrorFlip() {
+        return 0;
+    }
+
+
+    public int getRemoteControlDownwindow() {
+        return 0;
+    }
+
+
+    public int getRemoteControlDownwindowState() {
+        return 0;
+    }
+
+
+    public int getRemoteControlUpwindow() {
+        return 0;
+    }
+
+
+    public int getRemoteControlUpwindowState() {
+        return 0;
+    }
+
+
+    public int getRemoteCtlUnlockingState() {
+        return 0;
+    }
+
+
+    public int getRemoteDrivingOnlineState() {
+        return 0;
+    }
+
+
+    public int getRightHeadlampLevel() {
+        return 0;
+    }
+
+
+    public int getRightViewMirrorFlipAngle() {
+        return 0;
+    }
+
+
+    public int getSOCConfig() {
+        return 0;
+    }
+
+
+    public int getSOCTarget() {
+        return 0;
+    }
+
+
+    public int getSafeBeltInd() {
+        return 0;
+    }
+
+
+    public int getSafeWarnState() {
+        return 0;
+    }
+
+
+    public int getSeatHeatingState(int i) {
+        return 0;
+    }
+
+
+    public int getSeatHeatingState1(int i) {
+        return 0;
+    }
+
+
+    public int getSeatVentilatingState(int i) {
+        return 0;
+    }
+
+
+    public int getStartKeyState() {
+        return 0;
+    }
+
+
+    public int getStartOrPowerInd() {
+        return 0;
+    }
+
+
+    public int getSteerAssis() {
+        return 0;
+    }
+
+
+    public int getSteerAssistPermission() {
+        return 0;
+    }
+
+
+    public int getSteerBack() {
+        return 0;
+    }
+
+
+    public int getSteerPositionAutoReturn() {
+        return 0;
+    }
+
+
+    public int getSteerType() {
+        return 0;
+    }
+
+
+    public int getStopRemoteCtrlDriveInd() {
+        return 0;
+    }
+
+
+    public int getType() {
+        return 0;
+    }
+
+
+    public int getUnlockDownwindow() {
+        return 0;
+    }
+
+
+    public int getUnlockSettingOnlineState() {
+        return 0;
+    }
+
+
+    public int getVoiceINDLevel() {
+        return 0;
+    }
+
+
+    public int getWindscreenWiperOverhaulState(int i) {
+        return 0;
+    }
+
+
+    public int hasFeature(String str) {
+        return 0;
+    }
+
+
+    public boolean postEvent(int i, int i2, int i3, Object obj) {
+        return false;
+    }
+
+
+    public int queryAllStatus() {
+        return 0;
+    }
+
+
+    public int reset(int i, int i2) {
+        return 0;
+    }
+
+
+    public int restoreCarDefaultValue() {
+        return 0;
+    }
+
+
+    public int restoreDrDefaultValue() {
+        return 0;
+    }
+
+
+    public int sendBTMacToIKEY(byte[] bArr) {
+        return 0;
+    }
+
+
+    public int setACAutoAir(int i) {
+        return 0;
+    }
+
+
+    public int setACAutoWindLevel(int i) {
+        return 0;
+    }
+
+
+    public int setACBTWind(int i) {
+        return 0;
+    }
+
+
+    public int setACPauseCycle(int i) {
+        return 0;
+    }
+
+
+    public int setACTunnelCycle(int i) {
+        return 0;
+    }
+
+
+    public void setAllStatus() {
+
+    }
+
+
+    public int setAutoExternalRearMirrorFollowUpSwitch(int i) {
+        return 0;
+    }
+
+
+    public int setAutoLockSwitch(int i) {
+        return 0;
+    }
+
+
+    public int setAutoLockTime(int i) {
+        return 0;
+    }
+
+
+    public int setAutoRainWiperState(int i) {
+        return 0;
+    }
+
+
+    public int setBTCallState(int i) {
+        return 0;
+    }
+
+
+    public int setBackDoorElectricMode(int i) {
+        return 0;
+    }
+
+
+    public int setBackDoorOpenedHeight(int i) {
+        return 0;
+    }
+
+
+    public int setBackHomeLightDelay(int i) {
+        return 0;
+    }
+
+
+    public int setBackHomeLightDelayValue(int i) {
+        return 0;
+    }
+
+
+    public int setBackRowControl(int i) {
+        return 0;
+    }
+
+
+    public int setBasePadAutoRiseState(int i) {
+        return 0;
+    }
+
+
+    public int setCallState(int i) {
+        return 0;
+    }
+
+
+    public int setCarRecorderRecording(int i) {
+        return 0;
+    }
+
+
+    public int setChargingPort(int i) {
+        return 0;
+    }
+
+
+    public int setCourtesyLampTime(int i) {
+        return 0;
+    }
+
+
+    public int setDoorLock(int i) {
+        return 0;
+    }
+
+
+    public int setDriveDoorCloseInd(int i) {
+        return 0;
+    }
+
+
+    public int setDriverSeatAutoReturn(int i) {
+        return 0;
+    }
+
+
+    public int setDrivingRecorderSwitchState(int i) {
+        return 0;
+    }
+
+
+    public int setEnergyFeedback(int i) {
+        return 0;
+    }
+
+
+    public int setEngineOilExitUpdateState(int i) {
+        return 0;
+    }
+
+
+    public int setFormatMemoryCard(int i) {
+        return 0;
+    }
+
+
+    public int setHeadlampLevel(int i) {
+        return 0;
+    }
+
+
+    public int setIALArea(int i) {
+        return 0;
+    }
+
+
+    public int setIALBrightness(int i) {
+        return 0;
+    }
+
+
+    public int setIALColor(int i) {
+        return 0;
+    }
+
+
+    public int setIALInfo(int i, int i2, int i3, int i4) {
+        return 0;
+    }
+
+
+    public int setILDuration(int i) {
+        return 0;
+    }
+
+
+    public int setINSTheme(int i) {
+        return 0;
+    }
+
+
+    public int setInsideLightDoorState(int i) {
+        return 0;
+    }
+
+
+    public int setInsideRearMirrorScreenState(int i) {
+        return 0;
+    }
+
+
+    public int setInsideRearMirrorScreenSwitchState(int i) {
+        return 0;
+    }
+
+
+    public int setIntelligentVoiceIND(int i) {
+        return 0;
+    }
+
+
+    public int setKeyPowerLowInd(int i) {
+        return 0;
+    }
+
+
+    public int setLanguage(int i) {
+        return 0;
+    }
+
+
+    public int setLeaveCarPInd(int i) {
+        return 0;
+    }
+
+
+    public int setLeftHomeLightDelay(int i) {
+        return 0;
+    }
+
+
+    public int setLeftHomeLightDelayValue(int i) {
+        return 0;
+    }
+
+
+    public int setLeftViewMirrorFlipAngle(int i) {
+        return 0;
+    }
+
+
+    public int setLockCarRiseWindow(int i) {
+        return 0;
+    }
+
+
+    public int setLockOff(int i) {
+        return 0;
+    }
+
+
+    public int setLockReplayVideo(int i) {
+        return 0;
+    }
+
+
+    public int setLongPressUnlockWindow(int i) {
+        return 0;
+    }
+
+
+    public int setLowBatteryInd(int i) {
+        return 0;
+    }
+
+
+    public int setLowOilInd(int i) {
+        return 0;
+    }
+
+
+    public int setMcuPH2State(int i) {
+        return 0;
+    }
+
+
+    public int setMicroSwitchLockWindowState(int i) {
+        return 0;
+    }
+
+
+    public int setMicroSwitchUnlockWindowState(int i) {
+        return 0;
+    }
+
+
+    public int setMissKeyInd(int i) {
+        return 0;
+    }
+
+
+    public int setNightDRLightOnInd(int i) {
+        return 0;
+    }
+
+
+    public int setOverspeedLock(int i) {
+        return 0;
+    }
+
+
+    public int setOverspeedLockingState(int i) {
+        return 0;
+    }
+
+
+    public int setPM25Power(int i) {
+        return 0;
+    }
+
+
+    public int setPM25Switch(int i) {
+        return 0;
+    }
+
+
+    public int setPM25SwitchCheck(int i) {
+        return 0;
+    }
+
+
+    public int setPM25TimeCheck(int i) {
+        return 0;
+    }
+
+
+    public int setPadRotation(int i) {
+        return 0;
+    }
+
+
+    public int setParkBrakeInd(int i) {
+        return 0;
+    }
+
+
+    public int setRearViewMirrorAutoFoldMode(int i) {
+        return 0;
+    }
+
+
+    public int setRearViewMirrorFlip(int i) {
+        return 0;
+    }
+
+
+    public int setRemoteControlDownwindowState(int i) {
+        return 0;
+    }
+
+
+    public int setRemoteControlUpwindowState(int i) {
+        return 0;
+    }
+
+
+    public int setRemoteCtlUnlockingState(int i) {
+        return 0;
+    }
+
+
+    public int setRightViewMirrorFlipAngle(int i) {
+        return 0;
+    }
+
+
+    public int setSOCTarget(int i) {
+        return 0;
+    }
+
+
+    public int setSafeBeltInd(int i) {
+        return 0;
+    }
+
+
+    public int setSeatHeatingState(int i, int i2) {
+        return 0;
+    }
+
+
+    public int setSeatHeatingState1(int i, int i2) {
+        return 0;
+    }
+
+
+    public int setSeatVentilatingState(int i, int i2) {
+        return 0;
+    }
+
+
+    public int setStartOrPowerInd(int i) {
+        return 0;
+    }
+
+
+    public int setSteerAssis(int i) {
+        return 0;
+    }
+
+
+    public int setSteerPositionAutoReturn(int i) {
+        return 0;
+    }
+
+
+    public int setStopRemoteCtrlDriveInd(int i) {
+        return 0;
+    }
+
+
+    public int setVoiceINDLevel(int i) {
+        return 0;
+    }
+
+
+    public int setWindscreenWiperOverhaulState(int i, int i2) {
+        return 0;
+    }
+
+
+    public int takeDrivingRecorderPicture() {
+        return 0;
+    }
+
+
+    public int turnOffInsideLight() {
+        return 0;
+    }
+
+
+    public int voiceCtlBackDoor(int i) {
+        return 0;
+    }
+
+
+    public int getIALBrightness(int i) {
+        return 0;
+    }
+
+
+    public int getIALColor(int i) {
+        return 0;
+    }
+
+
+    public int setIALArea(int i, int i2) {
+        return 0;
+    }
+
+
+    public int setIALBrightness(int i, int i2, int i3) {
+        return 0;
+    }
+
+
+    public int setIALColor(int i, int i2, int i3) {
+        return 0;
+    }
+
+
+    public int turnOffInsideLight(int i) {
+        return 0;
+    }
+}
